b2c4d5330f674d39d4205400e5eacaec0be042d5
Guilherme Lima <ghduep@outlook.com>
2025-08-28 07:24:35 -0300

    adding pagination (15 per page)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index(): JsonResponse
     {
-        $posts = Post::all();
+        $posts = Post::paginate(15);
         return response()->json($posts);
     }
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index(): JsonResponse
     {
-        $users = User::all();
+        $users = User::paginate(15);
         return response()->json($users);
     }
 
